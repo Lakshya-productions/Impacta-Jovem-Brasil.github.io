a475964da21dced6e87d6808321f56a37a14f42a
Weslley <samuraiweslleyjack@gmail.com>
2020-05-11 19:14:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.1em solid #666 }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.1em solid black }";
   document.body.appendChild(css);
 };
